d2de8e13f9245663233a254ca282a22539e93489
decentninja <very@decent.ninja>
2017-11-18 23:24:24 +0100

    First spectrogram! Why is it mirrored?

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,10 @@ extern crate stft;
 use stft::{STFT, WindowType};
 
 
-const BINS: u32 = 500;
 
-pub fn draw<P: AsRef<std::path::Path>>(wav: P, image: P) {
-
-/*
+pub fn draw<P: AsRef<std::path::Path>>(wav: P, file_path: P) {
     let mut reader = hound::WavReader::open(wav).unwrap();
-    let sample_rate = reader.spec().sample_rate;
-    let samples: Vec<f64> = reader.samples().map(|x: ()| x.unwrap() as f64).collect();
-    */
-    let sample_rate: usize = 44100;
-    let seconds: usize = 10;
-    let sample_count = sample_rate * seconds;
-    let samples = (0..sample_count).map(|x| x as f64).collect::<Vec<f64>>();
+    let samples: Vec<f64> = reader.samples::<i16>().map(|x| x.unwrap() as f64).collect();
 
     let mut image_data: Vec<u8> = vec![];
     let mut image_lines = 0;
@@ -25,33 +16,30 @@ pub fn draw<P: AsRef<std::path::Path>>(wav: P, image: P) {
     let window_size: usize = 1024;
     let step_size: usize = 512;
     let mut stft = STFT::<f64>::new(window_type, window_size, step_size);
-    let mut spectrogram_column: Vec<f64> =
-        std::iter::repeat(0.).take(stft.output_size()).collect();
-    for some_samples in (&samples[..]).chunks(BINS as usize) {
+    let mut spectrogram_column: Vec<f64> = std::iter::repeat(0.).take(stft.output_size()).collect();
+    let mut max = std::f64::MIN;
+    let mut min = std::f64::MAX;
+    for some_samples in (&samples[..]).chunks(step_size as usize) {
         stft.append_samples(some_samples);
         while stft.contains_enough_to_compute() {
             stft.compute_column(&mut spectrogram_column[..]);
-            if some_samples.len() == window_size { // TODO: Remove this hack
-                for bin in &spectrogram_column {
-                    println!("{}", bin);
-                    image_data.push((bin / 1000.) as u8); // TODO: replace 1000 with something?
-                }
-                image_lines += 1;
+            for bin in &spectrogram_column {
+                max = f64::max(max, *bin);
+                min = f64::min(min, *bin);
+                let val = (bin / 8. * std::u8::MAX as f64) as u8;   // TODO: log or something? Why 8?
+                image_data.push(val);
             }
+            image_lines += 1;
             stft.move_to_next_column();
         }
     }
-    image::save_buffer(image, &image_data[..], BINS, image_lines, image::ColorType::Gray(8)).unwrap();
+    println!("max: {}, min: {}", max, min);
+    let spectrogram: image::GrayImage = image::ImageBuffer::from_raw(step_size as u32, image_lines as u32, image_data).expect("Could not create image");
+    let rotated = image::imageops::rotate270(&spectrogram);
+    rotated.save(file_path).expect("Could not write file");
 }
-
-/*
-#[test]
-fn it_works() {
-    draw("TTY.wav", "image.png")
-}
-*/
 
 #[test]
 fn violin() {
-    draw("Violin.wav", "image2.png")
+    draw("Violin.wav", "violin.png")
 }
